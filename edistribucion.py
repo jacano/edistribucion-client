eb7d56608b942af3cb21def212a5b8ae7c62f8d6
jacano <jacanosalado@gmail.com>
2026-09-15 11:25:19 +0200

    Accept only English answers in the save prompt

--- edistribucion.py
+++ edistribucion.py
@@ -816,7 +816,7 @@ def ask_yes_no(question, default=False):
         return default
     if not reply:
         return default
-    return reply in ("y", "yes", "s", "si", "sí")
+    return reply in ("y", "yes")
 
 
 def cmd_login(args):
